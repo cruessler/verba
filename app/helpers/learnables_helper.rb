9426bc2fa614ff62bdeda3764416c2487275d7b9
Christoph Rüßler <christoph.ruessler@gmail.com>
2014-05-08 23:25:20 +0200

    Move ratings helpers to RatingsHelper

--- app/helpers/learnables_helper.rb
+++ app/helpers/learnables_helper.rb
@@ -1,21 +1,2 @@
 module LearnablesHelper
-  def rate_link rating
-    link_to rating.to_s, '#', { class: 'btn btn-default rate-link pull-right', 'data-rating' => rating }
-  end
-  
-  def questions_left_for_today
-    current_user.ratings.scheduled_for_review.count
-  end
-  
-  def questions_left_with_bad_rating
-    current_user.ratings.with_bad_rating.count
-  end
-  
-  def average_rating
-    (current_user.ratings.average('rating') || 0).round(2)
-  end
-  
-  def next_review
-    (current_user.ratings.minimum('next_review') || Time.now).in_time_zone
-  end
 end
