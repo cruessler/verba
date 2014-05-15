30b981bab8a5acb9d66ff2ea5e3f28cdf8255b1b
Christoph Rüßler <christoph.ruessler@gmail.com>
2014-05-15 17:37:08 +0200

    Fix NoMethodError for pages without login
    Add test

--- app/helpers/learnables_helper.rb
+++ app/helpers/learnables_helper.rb
@@ -1,2 +1,5 @@
 module LearnablesHelper
+  def current_vocabulary
+    current_user.try(:current_vocabulary).try(:name)
+  end
 end
--- test/controllers/learnables_controller_test.rb
+++ test/controllers/learnables_controller_test.rb
@@ -1,4 +1,14 @@
 require 'test_helper'
 
 class LearnablesControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+  
+  test "should get overview" do
+    get :overview
+    assert_redirected_to new_user_session_path
+    
+    sign_in users(:one)
+    get :overview
+    assert_response :success
+  end
 end
