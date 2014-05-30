188962f434eaa4df8619d9897f5da2a003cf6596
Christoph Rüßler <christoph.ruessler@gmail.com>
2014-05-30 20:44:57 +0200

    Refactor Learnables#overview
    Use helpers instead of instance variables
    Reorder the information

--- app/controllers/learnables_controller.rb
+++ app/controllers/learnables_controller.rb
@@ -1,8 +1,6 @@
 class LearnablesController < ApplicationController
   # GET /learnables/overview
   def overview
-    @word_count = Word.count
-    @phrase_count = Phrase.count
     @questions_for_now = current_user.questions_for_the_next 0
     @questions_for_the_next_24_hours = current_user.questions_for_the_next 1.day
     @questions_for_the_next_48_hours = current_user.questions_for_the_next 2.days
--- app/helpers/learnables_helper.rb
+++ app/helpers/learnables_helper.rb
@@ -1,5 +1,9 @@
 module LearnablesHelper
-  def current_vocabulary
-    current_user.try(:current_vocabulary).try(:name)
+  def current_word_count
+    current_user.learnables.where([ 'type = ?', 'Word' ]).count || 0
+  end
+  
+  def current_phrase_count
+    current_user.learnables.where([ 'type = ?', 'Phrase' ]).count || 0
   end
 end
