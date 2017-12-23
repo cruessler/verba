75f6be5c745b4a450f2617ef3f27942813330fb7
Christoph Rüßler <christoph.ruessler@mailbox.org>
2017-12-23 18:48:12 +0100

    Run `rails db:migrate` after upgrade

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20140512162956) do
 
-  create_table "learnables", force: true do |t|
+  create_table "learnables", force: :cascade do |t|
     t.string   "type"
     t.string   "lemma"
     t.string   "long_lemma"
@@ -21,19 +20,17 @@ ActiveRecord::Schema.define(version: 20140512162956) do
     t.string   "translation"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["lemma"], name: "index_learnables_on_lemma"
   end
 
-  add_index "learnables", ["lemma"], name: "index_learnables_on_lemma"
-
-  create_table "learnables_vocabularies", id: false, force: true do |t|
+  create_table "learnables_vocabularies", id: false, force: :cascade do |t|
     t.integer "vocabulary_id", null: false
     t.integer "learnable_id",  null: false
+    t.index ["learnable_id"], name: "index_learnables_vocabularies_on_learnable_id"
+    t.index ["vocabulary_id"], name: "index_learnables_vocabularies_on_vocabulary_id"
   end
 
-  add_index "learnables_vocabularies", ["learnable_id"], name: "index_learnables_vocabularies_on_learnable_id"
-  add_index "learnables_vocabularies", ["vocabulary_id"], name: "index_learnables_vocabularies_on_vocabulary_id"
-
-  create_table "ratings", force: true do |t|
+  create_table "ratings", force: :cascade do |t|
     t.integer  "learnable_id"
     t.integer  "user_id"
     t.integer  "repetition"
@@ -43,14 +40,13 @@ ActiveRecord::Schema.define(version: 20140512162956) do
     t.integer  "rating"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["learnable_id"], name: "index_ratings_on_learnable_id"
+    t.index ["next_review"], name: "index_ratings_on_next_review"
+    t.index ["rating"], name: "index_ratings_on_rating"
+    t.index ["user_id"], name: "index_ratings_on_user_id"
   end
 
-  add_index "ratings", ["learnable_id"], name: "index_ratings_on_learnable_id"
-  add_index "ratings", ["next_review"], name: "index_ratings_on_next_review"
-  add_index "ratings", ["rating"], name: "index_ratings_on_rating"
-  add_index "ratings", ["user_id"], name: "index_ratings_on_user_id"
-
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -64,12 +60,11 @@ ActiveRecord::Schema.define(version: 20140512162956) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "current_vocabulary_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-  create_table "vocabularies", force: true do |t|
+  create_table "vocabularies", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
