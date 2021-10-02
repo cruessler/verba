da29f7155c141c653e5f5a879507f7edc0baa206
Christoph Rüßler <christoph.ruessler@mailbox.org>
2021-10-02 14:50:42 +0200

    Replace optimize-css-assets-webpack-plugin
    - Use `css-minimizer-webpack-plugin` instead. The previous plugin does
      not support Webpack 5.
    - Update `mini-css-extract-plugin`.

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => ({
   optimization: {
     minimizer: [
       new TerserPlugin({ cache: true, parallel: true, sourceMap: false }),
-      new OptimizeCSSAssetsPlugin({}),
+      new CssMinimizerPlugin({}),
     ],
   },
   entry: './app/assets/javascripts/application.js',
